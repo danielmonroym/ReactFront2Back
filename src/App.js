ace532b4a10ab700489993af050d216b718172b2
daniel.monroy <daniel.monroym@udea.edu.co>
2021-02-09 14:53:28 -0500

    Part 1, learning JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+   foo2 = () => 'Bar2';
+  render() {
+     const name = 'John  Doe';
+     const foo= () => 'Bar';
+     const loading = false;
+     const showName = true; 
+
+   
+
+    return (
+      <div className="App">
+      {loading ? <h4>Loading...</h4> :  <h1>Hello { showName && name.toUpperCase()} {1+1}</h1>
+
+
+    }
+          <h1>{foo()}</h1>
+          <h1>{this.foo2()}</h1>
+         </div>
+
+
+
+
+      
+ 
+    );
+  }
+
 }
 
 export default App;
