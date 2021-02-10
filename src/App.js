1872dc24e65cd118faaba4e42e1cec1d7ed861a7
daniel.monroy <daniel.monroym@udea.edu.co>
2021-02-10 09:41:39 -0500

    props and state

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users'
+import "./App.css";
 
 class App extends Component {
-   foo2 = () => 'Bar2';
+  
   render() {
-     const name = 'John  Doe';
-     const foo= () => 'Bar';
-     const loading = false;
-     const showName = true; 
-
-   
-
     return (
       <div className="App">
-      {loading ? <h4>Loading...</h4> :  <h1>Hello { showName && name.toUpperCase()} {1+1}</h1>
-
-
-    }
-          <h1>{foo()}</h1>
-          <h1>{this.foo2()}</h1>
-         </div>
-
-
-
-
+        <Navbar/>
+        <div className="container">
+        <Users/>
+        </div>
       
- 
+      </div>
     );
   }
-
 }
 
 export default App;
